a66a2792385ae8610ed6404a95ad1961c37c37c8
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-02-18 03:12:56 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * return - Always 0 (sucees)
+  */
+
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
