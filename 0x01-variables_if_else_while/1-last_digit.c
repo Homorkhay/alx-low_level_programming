4666d41156f325c9dc49847b97e76aa71c33facc
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-02-19 06:55:15 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	if (m == 0)
 		printf("last digit of %d is %d and is 0\n", n, m);
 	if (m < 6 && m != 0)
-		printf("last digit of %d is %d and is not less than 6 and not 0\n", n, m);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
 }
 
