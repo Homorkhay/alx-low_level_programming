f402ae9353872cce101f1954215b9377f8711dfa
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-02-20 03:50:43 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (success)
+  */
 
 int main(void)
 {
