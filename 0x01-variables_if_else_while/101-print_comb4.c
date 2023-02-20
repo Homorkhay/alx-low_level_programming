905e105e16d91b685b9729b6ff67ebf2f373c5b1
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-02-20 14:04:53 -0800

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * main prints all possible different combinations of three digits
+  * numbers must be seperated by comma followed by space
+  * The three digits must be different
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
+
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
