5dc3a60a19c28b257a96de84fd1a7f12780e07d3
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-02-22 04:59:40 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_last_digit - prints last digi of a number
+  * @n: parameter
+  * Return: Returns the value of the last digit
+  */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
+}
