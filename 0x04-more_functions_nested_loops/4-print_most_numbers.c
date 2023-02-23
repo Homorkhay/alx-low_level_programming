f0b4d440a6e27629d9730c356ca46bf414d295b4
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-02-23 07:25:15 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - print numbers from 0 to 9 in exception to 2 and 4
+  */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+	}
+	_putchar('\n');
+}
