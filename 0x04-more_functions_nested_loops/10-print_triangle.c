9e846fb8e3e3e13e32c0fd964a9478df527b1022
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-02-24 05:19:55 -0800

    10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * print_triangle - writes function that prints triangle
+  * @size: parameters to be cheacked
+  * Return: empty
+  */
+
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+		for (a = 0; a < size; a++)
+		{
+			for (b = size - a; b > 1; b--)
+			{
+				_putchar(32);
+			}
+			for (c = 0; c <= a; c++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+}
