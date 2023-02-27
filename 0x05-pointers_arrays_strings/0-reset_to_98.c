180d4d51bac6ca1658346db3ecd2a376b1e7ec05
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-02-27 02:12:53 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - writes function that takes a pointer
+  * @n: parametr to be updated
+  *
+  * Return: nothing on success
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
