431e39c53842a92a9358bd9392da26e7ac6753d9
Omokehinde happiness <omokehindehappiness589@gmail.com>
2023-03-03 16:40:42 +0000

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * leet - encodes a string
+  * @n: input
+  *
+  * Return: Returns the value of n
+  */
+
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEo0tTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
