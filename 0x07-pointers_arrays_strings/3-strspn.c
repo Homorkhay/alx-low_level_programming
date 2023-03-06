68769790ea1477327bb8fe34dfaeaa21c0113073
Omokehinde happiness <omokehindehappiness589@gmail.com>
2023-03-06 21:49:34 +0000

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _strspn - gets the length of a prefix substring
+  * @s: source string
+  * @accept: accepted string
+  *
+  * Return: return number of bytes in init segment
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0, b, t = 0;
+
+	while (accept[a])
+	{
+		b = 0;
+
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return(t);
+}
