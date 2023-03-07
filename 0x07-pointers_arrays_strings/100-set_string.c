23c6ae9dce507e6f31fa22a91d38a73949fcf73c
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-03-07 13:09:02 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * set_string - sets the value of a pointer to a char
+  * @s: value to modify
+  * @to: value to assign
+  *
+  * Return: Nothing on success
+  */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@ int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+void set_string(char **s, char *to);
 
 #endif
