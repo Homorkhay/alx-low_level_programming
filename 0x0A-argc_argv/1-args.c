38589fb0a43ec982149b41ab0a635234d26ce739
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-03-11 03:54:14 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - prints the number of args
+  * @argc: Argument count
+  * @argv: Argunent vector
+  *
+  * Return: Always zero
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
