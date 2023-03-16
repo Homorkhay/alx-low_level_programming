2d61a68970bdb983a0b7a7699b65e0c7c11d2209
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-03-16 12:02:30 -0700

    1-string_concat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 
 	while (s1[i])
 		i++;
