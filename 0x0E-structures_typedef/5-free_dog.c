13c80cde58ecc00def7243c36d6de392160de0e9
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-03-21 02:09:08 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+  * free_dog - frees dogs
+  * @d: Dog to be freed
+  *
+  * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+
+	free(d);
+}
