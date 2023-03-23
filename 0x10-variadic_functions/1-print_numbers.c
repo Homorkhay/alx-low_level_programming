8276c45103795826f94f4d5edf40c9720830b4e8
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-03-23 16:45:55 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  * print_numbers - print numbers followed by a new line
+  * @separator: is the string to abe peinted between two numbers
+  * @n: number of integers passed to the function
+  *
+  * Return: Nothing
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, unsigned int));
+		{
+			if (separator != NULL)
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
