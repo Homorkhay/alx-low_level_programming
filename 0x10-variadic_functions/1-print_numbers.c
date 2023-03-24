b0a546d91d08a477a5b53a88968451dddef051a8
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-03-23 17:02:52 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, unsigned int));
 		{
-			if (separator != NULL && i != n -1)
+			if (separator != NULL && i != n - 1)
 			{
 				printf("%s", separator);
 			}
