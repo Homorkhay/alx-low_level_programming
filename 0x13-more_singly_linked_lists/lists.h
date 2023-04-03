b3f3a017ec28f6c8f08239d24b1c76e4d187c0c8
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-04-03 13:41:56 -0700

    lists.h

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef _linked_list
+#define _linked_list
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+
+#endif
