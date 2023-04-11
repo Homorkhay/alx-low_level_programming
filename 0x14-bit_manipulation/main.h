ec2e86a2dacb742f1fa505918fe3fbece61a5ae8
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-04-11 00:49:32 -0700

    Header file for function prototypes

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 int _putchar(char c);
+void _divide(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
