c106349aa292683f7f4940ec358b8fa1a450a7e5
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-04-14 03:16:23 -0700

    add header file for function prototypes

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,7 @@
+#ifndef main_h
+#define main_h
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
