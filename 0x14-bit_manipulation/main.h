20d53a651fb36f14e93789d56d36c1469a054325
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-05-04 14:05:39 -0700

    add header file tt holds the function prototyoes for the project

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-int  _putchar(chr c);
+int  _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
