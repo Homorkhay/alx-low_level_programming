69426b0d7ee3a8b9c766c84d6fbd16d4556f0d16
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-05-08 08:09:30 -0700

    add function to read and print textfile to STDOUT

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _read_textfile - reads text file and print it to POSIX standard output
+* @filename: textfile to read
+* @letters: number of letters it should read and print
+*
+* Return: actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, bytes_read;
+	char *buffer = malloc(sizeof(char *) * letters);
+
+	if (buffer == NULL)
+		return (0);
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY, 0600);
+
+	if (file == -1)
+		return (0);
+	bytes_read = read(file, buffer, letters);
+
+	write(STDOUT_FILENO, buffer, bytes_read);
+
+	free(buffer);
+	close(file);
+	return (bytes_read);
+}
