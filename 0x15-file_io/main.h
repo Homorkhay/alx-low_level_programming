143660e7b1937c65fc5809e596a38d14b0f1fafe
Omokehinde Happiness <omokehindehappiness589@gmail.com>
2023-05-08 15:14:50 -0700

    Header file for the project

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
