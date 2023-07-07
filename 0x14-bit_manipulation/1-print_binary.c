87942fac0036d726e8fc4466890ff09a0a94ba20
Omokehinde Happines <omokehindehappiness589@gmail.com>
2023-07-07 14:14:04 -0700

    add function to print binary rep of unsigned int

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: The number to represent in binary
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	_divide(n);
+}
+
+/**
+  * _divide - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+		return;
+
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
