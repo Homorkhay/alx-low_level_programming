9b80363aed10d393976141befbceed84c3587423
Omokehinde happiness <omokehindehappiness589@gmail.com>
2023-07-07 18:39:38 +0000

    header file for my function prototypes

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h 
+#define main_h
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+
+#endif
