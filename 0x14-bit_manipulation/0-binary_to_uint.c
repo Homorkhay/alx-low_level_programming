9e03f4f7e04dcfb57573837c2905f856b83eb4d5
Omokehinde happiness <omokehindehappiness589@gmail.com>
2023-07-07 18:49:07 +0000

    add function to convert binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (b[i] != '\0')
 	{
-		if (b[i] == '0' || b[i] ==  '1')
+		if (b[i] == '0' || b[i] == '1')
 		{
 			num = num << 1;
 			if (b[i] == '1')
