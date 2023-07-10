acd34075023b836a8e8490eafa10d39fb6dcc9b7
Omokehinde Happines <omokehindehappiness589@gmail.com>
2023-07-10 09:54:25 -0700

    header file for my function prototypes

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef main_h
+#define  main_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
+
