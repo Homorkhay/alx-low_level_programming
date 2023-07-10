62a3f2f1bf095fd664486699e86f8e36089b14b9
Omokehinde Happines <omokehindehappiness589@gmail.com>
2023-07-10 10:42:35 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, readed;
-	char *buff = malloc(sizeof(char*) * letters);
+	char *buff = malloc(sizeof(char *) * letters);
 
 	if (!buff)
 		return (0);
