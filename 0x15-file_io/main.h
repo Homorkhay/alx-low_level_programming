c6af7073f8a931e89f48453bff19e70de5de6f50
Omokehinde Happines <omokehindehappiness589@gmail.com>
2023-07-10 19:09:41 -0700

    header file for function prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,6 +14,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *src, const char *dest);
 
 #endif
 
